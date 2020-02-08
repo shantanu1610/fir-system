3207c69fc284c784b5276f1f1a140120fa2cccfc
shantanu1610 <60808181+shantanu1610@users.noreply.github.com>
2020-02-08 16:30:51 +0530

    Delete urls.py

--- adhaar/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('login', views.login),
-    # path('test', views.logintest),
-    path('otp', views.otp),
-    path('welcome', views.welcome),
-]
