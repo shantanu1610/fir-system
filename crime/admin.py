4f3aa749de6329b8645354251d7708051ebe6190
shantanu1610 <60358688+shantanu16101998@users.noreply.github.com>
2020-02-08 16:36:39 +0530

    removed

--- crime/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Crime
-# Register your models here.
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title')
-
-
-admin.site.register(Crime)
--- crime/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Crime(models.Model):
-    title = models.CharField(max_length=200)
-    image_url = models.CharField(max_length=2100, default='url')
-    description = models.TextField()
